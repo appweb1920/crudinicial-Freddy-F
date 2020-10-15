1632bfa62d39c47c686ddbde9fbcb417e0c24e72
Freddy-F <freddy.mtz.1999@gmail.com>
2020-10-15 03:20:07 -0500

    Se crean migraciones (sin definir), modelos, controladores

---
+++ app/Http/Controllers/detalleRecolector_Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\detalleRecolector;
+use Illuminate\Http\Request;
+
+class detalleRecolector_Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\detalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function show(detalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\detalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(detalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\detalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, detalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\detalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(detalleRecolector $detalleRecolector)
+    {
+        //
+    }
+}
---
+++ app/recolectores.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class recolectores extends Model
+{
+    //
+}
