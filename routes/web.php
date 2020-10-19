753d475c3aecd19b23fb1a830bbccf5bc982d602
Freddy-F <freddy.mtz.1999@gmail.com>
2020-10-19 02:39:16 -0500

    PARCHES en rutas, ver detalles en rutas de web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('welcome'); });
 Route::get('/inicio', function () { return view('inicio'); });
+
 Route::get('/testBoostrap', function () { return view('boostrapTests'); });
 Route::get('/testMaterial', function () { return view('materialCSSTests'); });
 
@@ -30,16 +31,45 @@ Route::get('/recolectores', 'recolectores_Controller@index');
 Route::get('/borrarPunto/{id}', 'puntosDeReciclaje_Controller@destroy');
 Route::get('/borrarRecolector/{id}', 'recolectores_Controller@destroy');
 
+
+/*FIXME: PARCHES TEMPORALES (para "editarPunto"): (VER DETALLES EN LA PARTE DETALLADA MÁS ABAJO) */
+Route::get('/editarPunto/inicio', function () { return redirect('inicio'); });
+Route::get('/editarPunto/recolectores', function () { return redirect('recolectores'); });
+Route::get('/editarPunto/puntosRecoleccion', function () { return redirect('puntosRecoleccion'); });
+Route::get('/editarPunto/registraRecolector', function () { return redirect('registraRecolector'); });
+Route::get('/editarPunto/registraPuntoReciclaje', function () { return redirect('registraPuntoReciclaje'); });
+/*FIXME: PARCHES TEMPORALES (para "editarPunto"): (VER DETALLES EN LA PARTE DETALLADA MÁS ABAJO) */
+Route::get('/editarRecolector/inicio', function () { return redirect('inicio'); });
+Route::get('/editarRecolector/recolectores', function () { return redirect('recolectores'); });
+Route::get('/editarRecolector/puntosRecoleccion', function () { return redirect('puntosRecoleccion'); });
+Route::get('/editarRecolector/registraRecolector', function () { return redirect('registraRecolector'); });
+Route::get('/editarRecolector/registraPuntoReciclaje', function () { return redirect('registraPuntoReciclaje'); });
+
 Route::get('/editarPunto/{id}', 'puntosDeReciclaje_Controller@edit');
 Route::get('/editarRecolector/{id}', 'recolectores_Controller@edit');
 
 Route::post('/editarPunto/editarPunto', 'puntosDeReciclaje_Controller@update');
 Route::post('/editarRecolector/editarRecolector', 'recolectores_Controller@update');
 
-Route::get('/detallesRecolector/recolectores', 'recolectores_Controller@index');
+/* FIXME: ¿COMO SE PUEDE HACER UN REDIRECCIONAMIENTO CORRECTO HACIA UN ELEMENTO DE LA BARRA?*/
+/*NOTA: "PARCHES": Evita un mal direccionamiento desde detallesRecolector hacia cualquier 
+                    punto en la barra de navegacion.    
+        Ejemplo: redireccion de DetallesRecolector a Recolectores (hacia atras), sin generar 
+        una vista sobre la direccion actual
+  NOTA: #2 Este conjunto de rutas deben estar ANTES DE LAS DE ABAJO , de lo contrario no funcionan.
+        FIXME: ¿Como influye el orden de declaracion de rutas en el enrutamiento?
+*/
+Route::get('/detallesRecolector/inicio', function () { return redirect('inicio'); });
+Route::get('/detallesRecolector/recolectores', function () { return redirect('recolectores'); });
+Route::get('/detallesRecolector/puntosRecoleccion', function () { return redirect('puntosRecoleccion'); });
+Route::get('/detallesRecolector/registraRecolector', function () { return redirect('registraRecolector'); });
+Route::get('/detallesRecolector/registraPuntoReciclaje', function () { return redirect('registraPuntoReciclaje'); });
+
 Route::get('/detallesRecolector/{id}', 'detalleRecolector_Controller@show');
 Route::get('/detallesRecolector/borrarPuntoRecolector/{idRecolector}/{idPunto}', 'detalleRecolector_Controller@destroy');
 Route::get('/detallesRecolector/asignarPuntoRecolector/{idRecolector}/{idPunto}', 'detalleRecolector_Controller@store');
+
+
 
 
 /* TODO: AGREGAR: 
@@ -47,5 +77,5 @@ Route::get('/detallesRecolector/asignarPuntoRecolector/{idRecolector}/{idPunto}'
 -[x] PANTALLAS DE CREACION ("PUNTOS","RECOLECTORES")
 -[x] PANTALLAS DE LISTADO ("PUNTOS","RECOLECTORES")
 -[x] PANTALLAS DE EDICION ("PUNTOS","RECOLECTORES")
--[] En la PANTALLAS DE LISTADO ("RECOLECTOR") agregar "Detalles" se permite agregar CREAR UNA RELACION ("RECOLECTORES" -> "PUNTOS")
+-[x] En la PANTALLAS DE LISTADO ("RECOLECTOR") agregar "Detalles" se permite agregar CREAR UNA RELACION ("RECOLECTORES" -> "PUNTOS")
  */
